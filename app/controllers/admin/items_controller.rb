c764e9962b96ab2ec0b13903dfe4e7cce7787c4d
Kei Hayashi <kei.h.hayashi@gmail.com>
2015-01-13 13:01:28 +0900

    add admin/items' features

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,58 @@
+class Admin::ItemsController < Admin::BaseController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  # GET /admin/items
+  def index
+    @items = Item.all
+  end
+
+  # GET /admin/items/1
+  def show
+  end
+
+  # GET /admin/items/new
+  def new
+    @item = Item.new
+  end
+
+  # GET /admin/items/1/edit
+  def edit
+  end
+
+  # POST /items
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to admin_item_path(@item), notice: 'Item was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /items/1
+  def update
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item), notice: 'Item was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /items/1
+  def destroy
+    @item.destroy
+    redirect_to admin_items_url, notice: 'Item was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def item_params
+      params.require(:item).permit(:name, :grade, :department)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root 'base#index'
     resources :users
+    resources :items
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
