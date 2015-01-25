2eff3bfe5558df7fca98c4b9d4cbc70368c69060
Kei Hayashi <kei.h.hayashi@gmail.com>
2015-01-25 11:14:02 +0900

    add negative ratings load to previous seed

--- db/seeds/previous_experiment.rb
+++ db/seeds/previous_experiment.rb
@@ -5,6 +5,7 @@ ratings  = JSON.parse(File.read("#{previous_experiment_path}/ratings.json"))
 opinions = JSON.parse(File.read("#{previous_experiment_path}/opinions.json"))
 user_keys = {'101' => 'F2', '91' => 'I2', '81' => 'K2', '71' => 'K1', '61' => 'I1', '51' => 'K2', '41' => 'M2', '31' => 'H1', '21' => 'Y1', '11' => 'Y2'}
 
+# load ratings
 ratings.each do |rating|
   # get user
   user_name = user_keys[rating['user_id']]
@@ -25,3 +26,22 @@ ratings.each do |rating|
 end
 
 puts "FINISH: #{ratings.size} ratings"
+
+# load negative ratings
+subjects.each do |s|
+  ItemDetail.create(name: s["title"])
+end
+
+opinions.each do |o|
+  user_id, item_id, subject_id = o["user_id"], o["item_id"], o["subject_id"]
+  user_name = user_keys[o['user_id']]
+  item_json = items.find{|i| i['id'] == o['item_id']}
+  user = User.find_by(name: user_name)
+  item = Item.find_by(name: item_json['name'], year: item_json['year'])
+  rating = Rating.find_by(user: user, item: item)
+  subject = subjects.find{|s| s["id"] == subject_id }
+  item_detail = ItemDetail.find_by(name: subject["title"])
+  RatingDetail.create!(user: user, item: item, rating: rating, item_detail: item_detail, negative_cause: true)
+end
+
+puts "FINISH: #{opinions.size} negative ratings"
